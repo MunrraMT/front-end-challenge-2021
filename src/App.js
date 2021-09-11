a014669b81df7f1383a322410d69a9e20f0bbb3e
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-11 12:27:42 -0400

    Header HTML Base

--- src/App.js
+++
@@ -1,3 +0,0 @@
-const App = () => <h1>Teste</h1>;
-
-export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+const Header = () => (
+  <header>
+    <img src="./logo.svg" alt="Logo da Empresa" width="50" height="50" />
+    <h1>Pharma Inc</h1>
+    <img
+      src="./perfil.webp"
+      alt="Foto de perfil do usuário"
+      width="50"
+      height="50"
+    />
+  </header>
+);
+
+export default Header;
