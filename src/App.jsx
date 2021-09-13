557fc0effd694ee294bfbd7fdb2b08b191e5ad72
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-13 18:29:47 -0400

    Fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useContext, useEffect } from 'react';
+
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import DataContext from './providers/DataContext';
 
-const App = () => (
-  <>
-    <Header />
-    <MainContent />
-  </>
-);
+const App = () => {
+  const context = useContext(DataContext);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?page=1&results=50`)
+      .then((response) => response.json())
+      .then(({ results }) => context.setData(results));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+};
 
 export default App;
