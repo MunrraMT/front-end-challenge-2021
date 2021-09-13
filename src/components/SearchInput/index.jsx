81aae3ae04305dee958437ec42ec5818e0c8491e
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-13 09:57:02 -0400

    Add marginY

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,28 +1,54 @@
 import { Box, InputAdornment, TextField } from '@material-ui/core';
-import { string } from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { number, string } from 'prop-types';
 
-const SearchInput = ({ label }) => (
-  <Box component="section" display="flex" justifyContent="center">
-    <TextField
-      size="small"
-      fullWidth
-      id="outlined-search"
-      label={label}
-      type="search"
-      variant="outlined"
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <img src="./search.svg" alt="teste" width="30" height="30" />
-          </InputAdornment>
-        ),
-      }}
-    />
-  </Box>
-);
+const useStyles = makeStyles(() => ({
+  marginBottom: (props) => ({
+    marginBottom: `${props.marginBottom}rem`,
+  }),
+  marginTop: (props) => ({
+    marginTop: `${props.marginTop}rem`,
+  }),
+  input: {
+    borderColor: '#209cee',
+    backgroundColor: '#ffffff',
+  },
+}));
+
+const SearchInput = ({ label, marginTop, marginBottom }) => {
+  const classes = useStyles({ marginTop, marginBottom });
+
+  return (
+    <Box component="section" display="flex" justifyContent="center">
+      <TextField
+        className={`${classes.marginBottom} ${classes.marginTop} ${classes.input}`}
+        size="small"
+        fullWidth
+        id="outlined-search"
+        label={label}
+        type="search"
+        variant="outlined"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <img src="./search.svg" alt="teste" width="30" height="30" />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
+  );
+};
+
+SearchInput.defaultProps = {
+  marginBottom: 0,
+  marginTop: 0,
+};
 
 SearchInput.propTypes = {
   label: string.isRequired,
+  marginBottom: number,
+  marginTop: number,
 };
 
 export default SearchInput;
