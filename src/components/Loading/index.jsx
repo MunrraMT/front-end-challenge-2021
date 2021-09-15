da97a2655d8a4d544ed14220ee73260a7920a2df
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-15 12:15:45 -0400

    New Loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,5 +1,14 @@
+import { Box } from '@material-ui/core';
+
 const Loading = () => (
-  <img width="50" height="50" src="/Reload-1s-50px.svg" alt="Loading" />
+  <Box
+    component="section"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <img width="50" height="50" src="/Reload-1s-50px.svg" alt="Loading" />
+  </Box>
 );
 
 export default Loading;
