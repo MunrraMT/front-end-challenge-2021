10c058845e3ab2325251569d89210cd641baee6f
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-16 11:24:13 -0400

    Moved FetchAPI from App to HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,28 @@
-import { useContext, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import DataContext from './providers/DataContext';
 import HomePage from './pages/HomePage';
 import PatientsDetails from './components/PatientDetails';
 import theme from './theme';
 
-const App = () => {
-  const { setData, setError } = useContext(DataContext);
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/patient/:seed-:page-:uuid"
+          component={PatientsDetails}
+        />
 
-  useEffect(() => {
-    fetch(`https://randomuser.me/api/?page=0&results=50`)
-      .then((response) => {
-        if (!response.ok) setError(true);
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
 
-        return response.json();
-      })
-      .then((dataFetch) => {
-        const newData = dataFetch.results.map((patient) => ({
-          ...patient,
-          page: 0,
-          seed: dataFetch.info.seed,
-        }));
-
-        setData(newData);
-      });
-  }, []);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/patient/:seed-:page-:uuid"
-            component={PatientsDetails}
-          />
-
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-
-        <Route path="/" component={HomePage} />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
+      <Route path="/" component={HomePage} />
+    </BrowserRouter>
+  </ThemeProvider>
+);
 
 export default App;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,36 @@
+import { useContext, useEffect } from 'react';
+
+import DataContext from '../../providers/DataContext';
 import Header from '../../components/Header';
 import MainContent from '../../components/MainContent';
 
-const HomePage = () => (
-  <>
-    <Header />
-    <MainContent />
-  </>
-);
+const HomePage = () => {
+  const { setData, setError } = useContext(DataContext);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?page=0&results=50`)
+      .then((response) => {
+        if (!response.ok) setError(true);
+
+        return response.json();
+      })
+      .then((dataFetch) => {
+        const newData = dataFetch.results.map((patient) => ({
+          ...patient,
+          page: 0,
+          seed: dataFetch.info.seed,
+        }));
+
+        setData(newData);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+};
 
 export default HomePage;
